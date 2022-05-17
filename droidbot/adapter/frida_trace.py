27ce80eceec19436098e26f2e38a73fb487b57f3
raphsimon <ra.simon1998@gmail.com>
2022-05-17 21:09:58 +0200

    Added default value to out

--- droidbot/adapter/frida_trace.py
+++ droidbot/adapter/frida_trace.py
@@ -13,14 +13,16 @@ class FridaTrace(Adapter):
     """
     Class to set the frida command to execute.
     """
-    def __init__(self, device=None , package_name=None, target_functions=None, output=None):
+    def __init__(self, device=None , package_name=None, target_functions=None, output="frida_out"):
         self.logger = logging.getLogger(self.__class__.__name__)
 
         self.device = device
 
         self.target_app = package_name
         self.target_functions = target_functions
-        self.output = output if output is not None else PIPE
+        self.output = output
+        print(self.output)
+        input()
         
         self.command = []
         self.process = None
@@ -30,26 +32,29 @@ class FridaTrace(Adapter):
 
 
     def build_command(self):
-        if self.target_app and self.target_functions:        
-            self.command.append("frida-trace", "-U", "-O", self.target_functions,
-                                "-f", self.target_app,
-                                "-o", self.output)
+        if self.target_app and self.target_functions:
+            self.command = ["frida-trace", "-U", "-O", self.target_functions,
+                            "-f", self.target_app,
+                            "-o", self.output]
         else:
-            self.logger.error("Package name is missing for frida-trace")
+            if not self.target_app:
+                self.logger.error("Package name is missing for frida-trace")
+            if not self.target_functions:
+                self.logger.error("No trace arguments provided")
 
 
     def start_tracing(self):
         self.build_command()
         if self.command and self.ready:
             self.logger.info(f'Running frida-trace command: {self.command}')
-            self.process = Popen(self.command, self.output)
+            self.process = Popen(self.command, stderr=PIPE)
         else:
             if not self.command:
                 self.logger.error("Command is not set")
             else:
                 self.logger.error("frida-server is not running on the device")
 
-            
+
     def check_connectivity(self):
         # Here we can se whether or not frida-server is running on the device
         self.ready = self.device.check_frida_server_state()
@@ -59,6 +64,7 @@ class FridaTrace(Adapter):
     def disconnect(self):
         if self.process:
             self.process.kill()
+
 
     def set_app_to_trace(self, package_name):
         self.target_app = package_name
